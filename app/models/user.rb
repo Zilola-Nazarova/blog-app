6e5398fcbd12eb8b41e065085a3ec26498af1615
zilola <zilolanazarova1994@gmail.com>
2023-10-25 19:48:42 +0500

    Add validations to Post and User classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comment
   has_many :like
 
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def three_recent_posts
     Post.where(author: self).order(created_at: :desc).first(3)
   end
