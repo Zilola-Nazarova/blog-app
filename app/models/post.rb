db5b34c3f6c115aaa9c886ab281d5ea6497fcf3c
zilola <zilolanazarova1994@gmail.com>
2023-10-25 16:11:13 +0500

    Add after_save active callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_many :comment
   has_many :like
   belongs_to :author, class_name: 'User'
+  after_save :update_posts_counter
 
   def five_recent_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
