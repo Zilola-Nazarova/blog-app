731add8e7be76900dacab514a209de03d1fc6c8e
zilola <zilolanazarova1994@gmail.com>
2023-10-25 15:00:36 +0500

    Write methods to display 5 recent comments, update comments counter and likes counter

--- app/models/concerns/post.rb
+++ app/models/concerns/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   has_many :comment
   has_many :like
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  
+  def five_recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
+
+  def update_comments_counter
+    update(comments_counter: Comment.where(post_id: id).count)
+  end
+
+  def update_likes_counter
+    update(likes_counter: Like.where(post_id: id).count)
+  end
 end
