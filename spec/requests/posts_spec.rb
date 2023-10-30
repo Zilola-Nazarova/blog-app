b084f2c5064311f52828d614e421dc93ae0ae9b4
zilola <zilolanazarova1994@gmail.com>
2023-10-30 14:45:57 +0500

    Add #before hook to make tests more consice

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   context 'GET /index' do
-    it 'returns successful response' do
+    before :each do
       get '/users/:user_id/posts'
+    end
+
+    it 'returns successful response' do
       expect(response).to be_successful
     end
 
     it 'returns http status 200' do
-      get '/users/:user_id/posts'
       expect(response.status).to eq(200)
     end
 
     it 'renders the right view file' do
-      get '/users/:user_id/posts'
       expect(response).to render_template(:index)
     end
 
     it 'renders the right placeholder' do
-      get '/users/:user_id/posts'
       expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
     end
   end
@@ -26,28 +26,25 @@ RSpec.describe 'Posts', type: :request do
   context 'GET /show' do
     let(:user) { User.create(name: 'Tom') }
     let(:valid_attributes) { { 'author' => user, 'title' => 'Title' } }
+    let(:post) { Post.create! valid_attributes }
+
+    before :each do
+      get "/users/:user_id/posts/#{post.id}"
+    end
 
     it 'returns successful response' do
-      post = Post.create! valid_attributes
-      get "/users/:user_id/posts/#{post.id}"
       expect(response).to be_successful
     end
 
     it 'returns http status 200' do
-      post = Post.create! valid_attributes
-      get "/users/:user_id/posts/#{post.id}"
       expect(response.status).to eq(200)
     end
 
     it 'renders the right view file' do
-      post = Post.create! valid_attributes
-      get "/users/:user_id/posts/#{post.id}"
       expect(response).to render_template(:show)
     end
 
     it 'renders the right placeholder' do
-      post = Post.create! valid_attributes
-      get "/users/:user_id/posts/#{post.id}"
       expect(response.body).to include('<h1>Here is a selected post for a given user</h1>')
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,51 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   context 'GET /index' do
-    it 'returns successful response' do
+    before :each do
       get users_path
+    end
+
+    it 'returns successful response' do
       expect(response).to be_successful
     end
 
     it 'returns http status 200' do
-      get users_url
       expect(response.status).to eq(200)
     end
 
     it 'renders the right view file' do
-      get users_url
       expect(response).to render_template(:index)
     end
 
     it 'renders the right placeholder' do
-      get users_url
       expect(response.body).to include('<h1>Here is a list of users</h1>')
     end
   end
 
   context 'GET /show' do
     let(:valid_attributes) { { 'name' => 'Tom' } }
+    let(:user) { User.create! valid_attributes }
+    before :each do
+      get user_url(user)
+    end
 
     it 'returns successful response' do
-      user = User.create! valid_attributes
-      get user_url(user)
       expect(response).to be_successful
     end
 
     it 'returns http status 200' do
-      user = User.create! valid_attributes
-      get user_url(user)
       expect(response.status).to eq(200)
     end
 
     it 'renders the right view file' do
-      user = User.create! valid_attributes
-      get user_url(user)
       expect(response).to render_template(:show)
     end
 
     it 'renders the right placeholder' do
-      user = User.create! valid_attributes
-      get user_url(user)
       expect(response.body).to include('<h1>Here is a selected user</h1>')
     end
   end
