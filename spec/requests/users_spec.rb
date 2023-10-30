d4e1053093fe58f1e2c4ab37a6a7d2a123c793be
zilola <zilolanazarova1994@gmail.com>
2023-10-30 14:34:06 +0500

    Write test cases for UserController#show method to check response, rendered view and placeholder

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include('<h1>Here is a list of users</h1>')
     end
   end
+
+  context "GET /show" do
+    let(:valid_attributes) { { "name" => "Tom" } }
+
+    it "returns successful response" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
+    end
+
+    it "returns http status 200" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the right view file" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to render_template(:show)
+    end
+
+    it "renders the right placeholder" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response.body).to include('<h1>Here is a selected user</h1>')
+    end
+  end
 end
