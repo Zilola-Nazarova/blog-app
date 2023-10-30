f7c11256f40671f113b31e8f6e5aaa47f4e7934b
zilola <zilolanazarova1994@gmail.com>
2023-10-30 14:36:36 +0500

    Write test cases for PostsController#show method to check response, rendered view and placeholder

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -21,5 +21,34 @@ RSpec.describe "Posts", type: :request do
       get '/users/:user_id/posts'
       expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
     end
+    
+  context "GET /show" do
+    let(:user) { User.create(name: 'Tom') }
+    let(:valid_attributes) { { 'author' => user, 'title' => 'Title'} }
+
+    it "returns successful response" do
+      post = Post.create! valid_attributes
+      get "/users/:user_id/posts/#{post.id}"
+      expect(response).to be_successful
+    end
+
+    it "returns http status 200" do
+      post = Post.create! valid_attributes
+      get "/users/:user_id/posts/#{post.id}"
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the right view file" do
+      post = Post.create! valid_attributes
+      get "/users/:user_id/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it "renders the right placeholder" do
+      post = Post.create! valid_attributes
+      get "/users/:user_id/posts/#{post.id}"
+      expect(response.body).to include('<h1>Here is a selected post for a given user</h1>')
+    end
+  end
   end
 end
