b05ada140911dbde1441dc4fc913fba817709ea8
zilola <zilolanazarova1994@gmail.com>
2023-10-30 14:41:20 +0500

    Fix rubocop erros: single-quotes/double-quotes, IndentationConsistency, OrderedGems, TrailingWhitespace

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,54 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  context "GET /index" do
-    it "returns successful response" do
+RSpec.describe 'Posts', type: :request do
+  context 'GET /index' do
+    it 'returns successful response' do
       get '/users/:user_id/posts'
       expect(response).to be_successful
     end
 
-    it "returns http status 200" do
+    it 'returns http status 200' do
       get '/users/:user_id/posts'
       expect(response.status).to eq(200)
     end
-    
-    it "renders the right view file" do
+
+    it 'renders the right view file' do
       get '/users/:user_id/posts'
       expect(response).to render_template(:index)
     end
 
-    it "renders the right placeholder" do
+    it 'renders the right placeholder' do
       get '/users/:user_id/posts'
       expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
     end
-    
-  context "GET /show" do
-    let(:user) { User.create(name: 'Tom') }
-    let(:valid_attributes) { { 'author' => user, 'title' => 'Title'} }
+  end
 
-    it "returns successful response" do
+  context 'GET /show' do
+    let(:user) { User.create(name: 'Tom') }
+    let(:valid_attributes) { { 'author' => user, 'title' => 'Title' } }
+
+    it 'returns successful response' do
       post = Post.create! valid_attributes
       get "/users/:user_id/posts/#{post.id}"
       expect(response).to be_successful
     end
 
-    it "returns http status 200" do
+    it 'returns http status 200' do
       post = Post.create! valid_attributes
       get "/users/:user_id/posts/#{post.id}"
       expect(response.status).to eq(200)
     end
 
-    it "renders the right view file" do
+    it 'renders the right view file' do
       post = Post.create! valid_attributes
       get "/users/:user_id/posts/#{post.id}"
       expect(response).to render_template(:show)
     end
 
-    it "renders the right placeholder" do
+    it 'renders the right placeholder' do
       post = Post.create! valid_attributes
       get "/users/:user_id/posts/#{post.id}"
       expect(response.body).to include('<h1>Here is a selected post for a given user</h1>')
     end
-  end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,50 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  context "GET /index" do
-    it "returns successful response" do
+RSpec.describe 'Users', type: :request do
+  context 'GET /index' do
+    it 'returns successful response' do
       get users_path
       expect(response).to be_successful
     end
 
-    it "returns http status 200" do
+    it 'returns http status 200' do
       get users_url
       expect(response.status).to eq(200)
     end
-    
-    it "renders the right view file" do
+
+    it 'renders the right view file' do
       get users_url
       expect(response).to render_template(:index)
     end
 
-    it "renders the right placeholder" do
+    it 'renders the right placeholder' do
       get users_url
       expect(response.body).to include('<h1>Here is a list of users</h1>')
     end
   end
 
-  context "GET /show" do
-    let(:valid_attributes) { { "name" => "Tom" } }
+  context 'GET /show' do
+    let(:valid_attributes) { { 'name' => 'Tom' } }
 
-    it "returns successful response" do
+    it 'returns successful response' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to be_successful
     end
 
-    it "returns http status 200" do
+    it 'returns http status 200' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response.status).to eq(200)
     end
 
-    it "renders the right view file" do
+    it 'renders the right view file' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to render_template(:show)
     end
 
-    it "renders the right placeholder" do
+    it 'renders the right placeholder' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response.body).to include('<h1>Here is a selected user</h1>')
