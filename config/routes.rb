11c3f8801628d627d9d9b9abf395660f55597f0c
zilola <zilolanazarova1994@gmail.com>
2023-11-06 18:48:48 +0500

    Redirect users to different root paths depending on their authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'pages#login', as: :unauthenticated_root
+    end
+  end
+
+  get '/login', to: 'pages#login'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
