addecdbe5a98e419caa6dbfb5c20903c78c6a25b
zilola <zilolanazarova1994@gmail.com>
2023-12-24 21:58:10 +0500

    Create application_controller that would authenticate every request by checking it's token

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,3 +1,14 @@
-class Api::ApplicationController < ActionController::API
+class Api::ApplicationController < ActionController::API 
+  include JsonWebToken
+  before_action :authenticate_request
   respond_to :json
+
+  private
+
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
+  end
 end
