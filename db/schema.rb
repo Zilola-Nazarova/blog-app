122f6869e112dafae4638f38f1210f362da7b9de
zilola <zilolanazarova1994@gmail.com>
2023-12-24 21:51:33 +0500

    Reset the database, run all modified migrations

--- db/schema.rb
+++ db/schema.rb
@@ -66,9 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_201047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "comments", "posts", on_delete: :cascade
+  add_foreign_key "comments", "users", on_delete: :cascade
+  add_foreign_key "likes", "posts", on_delete: :cascade
+  add_foreign_key "likes", "users", on_delete: :cascade
+  add_foreign_key "posts", "users", column: "author_id", on_delete: :cascade
 end
