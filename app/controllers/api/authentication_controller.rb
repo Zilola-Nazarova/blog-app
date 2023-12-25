b9573ad8f82074d60321c97a896ab8c327a35837
zilola <zilolanazarova1994@gmail.com>
2023-12-25 14:12:29 +0500

    Create authentication_controller to perform the login action

---
+++ app/controllers/api/authentication_controller.rb
@@ -0,0 +1,14 @@
+class Api::AuthenticationController < Api::ApplicationController
+  skip_before_action :authenticate_request
+
+  # POST /auth/login
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.valid_password?(params[:password])
+      token = jwt_encode(user_id: @user.id)
+      render json: {token: token }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+end
