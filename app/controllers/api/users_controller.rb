85a45615b847de715d0ea57fb771a7fc2f939578
zilola <zilolanazarova1994@gmail.com>
2023-12-25 14:21:17 +0500

    Create users_controller with different actions to perform them via API requests

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,48 @@
+class Api::UsersController < Api::ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: [:show, :destroy]
+  
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  # GET /users/{username}
+  def show
+    render json: @user, status: :ok
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  # PUT /users/{username}
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+              status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/{username}
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
