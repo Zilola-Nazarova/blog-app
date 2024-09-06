9ad2ee12a3b94cbd2150f7debb68d0885151af2f
zilola <zilolanazarova1994@gmail.com>
2024-09-06 07:06:13 +0500

    Activate mailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,18 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'blog-app-bekk.onrender.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            "<%= Rails.application.credentials.gmail_username %>",
+    password:             "<%= Rails.application.credentials.gmail_password %>",
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
